5398522eff991454dce74949d0999442062981e7
resumerise <59031801+resumerise@users.noreply.github.com>
2021-10-06 12:11:55 +0200

    update library version (#3)
    Co-authored-by: Elton M <eltonmarku@gmail.com>

--- dev.sh
+++ dev.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-deno run -L debug --unstable --reload --import-map=local_import_maps.json --watch -A index.ts serve /Users/eltonmarku/resumerise/resumerise_flux/resumerise_theme_retro/mod.ts
+deno run --unstable --import-map=local_import_maps.json --watch -A index.ts serve file:///Users/eltonmarku/resumerise/resumerise_flux/resumerise_theme_retro/mod.ts
