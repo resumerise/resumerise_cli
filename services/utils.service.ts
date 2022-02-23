06b84a7858564621f9cdd7a0d09b2bcef158364f
Elton M <eltonmarku@gmail.com>
2022-02-23 09:54:00 +0100

    open preview url after server has started

--- services/utils.service.ts
+++ services/utils.service.ts
@@ -1,10 +1,13 @@
 import app from "../server/app.ts";
+import { open } from "opener/mod.ts";
 
 export class UtilsService {
   async startApp() {
-    app.addEventListener("listen", ({ hostname, port, secure }) => {
-      console.log("server has started on http://localhost:3000/html 🚀");
+    app.addEventListener("listen", async ({ hostname, port, secure }) => {
+      const previewUrl = "http://localhost:3000/html?type=DESKTOP";
+      console.log(`server has started on ${previewUrl} 🚀`);
       console.log("Happy coding !");
+      await open(previewUrl);
     });
 
     await app.listen({ port: 3000 });
